2c747c90af187736117ac8b80cda8aaa40ffe0fe
Mathew B <bogaert@email.arizona.edu>
2021-01-04 14:24:03 -0700

    need help

--- models/User.js
+++ models/User.js
@@ -8,6 +8,31 @@ class User extends Model {}
 User.init(
     {
         // TABLE COLUMN DEFINITIONS GO HERE
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          username: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true
+            }
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [4]
+            }
+          }
     },
     {
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
